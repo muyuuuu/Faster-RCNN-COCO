184300d5ffbacf86489f22897325912b90c76b91
muyuuuu <muyu0223@outlook.com>
2021-05-06 15:40:28 +0800

    reconstruct anno json for torch batchsize

---
+++ reconstruct-anno.py
@@ -0,0 +1,60 @@
+'''
+File: refactor-anno.py
+Project: Faster-RCNN-COCO
+File Created: Thursday, 6th May 2021 3:13:55 pm
+Author: lanling (https://github.com/muyuuuu)
+-----------
+Last Modified: Thursday, 6th May 2021 3:38:04 pm
+Modified By: lanling (https://github.com/muyuuuu)
+Copyright 2020 - 2021 XDU, XDU
+-----------
+Description: 为了 torch 的 batch size 能对齐，重新生成 json
+'''
+
+import json
+from pycocotools.coco import COCO
+
+anno_path = 'data/openbrand_train.json'
+coco = COCO(anno_path)
+ids = list(coco.imgs.keys())
+image_dir = 'data/train_data/'
+result = []
+
+for idx in range(len(ids)):
+    img_id = ids[idx]
+
+    ann_ids = coco.getAnnIds(imgIds=img_id)
+    target = coco.loadAnns(ann_ids)
+
+    img_ids = coco.getImgIds(imgIds=img_id)
+    image = coco.loadImgs(img_ids)
+
+    for i in target:
+        d = {}
+        for key in i.keys():
+            if key == 'iscrowd':
+                d['iscrowd'] = i[key]
+            elif key == 'image_id':
+                d['image_id'] = i[key]
+            elif key == 'bbox':
+                d['boxes'] = i[key]
+            elif key == 'category_id':
+                d['category_id'] = i[key]
+            elif key == 'area':
+                d['area'] = i[key]
+            elif key == 'image_id':
+                d['image_id'] = i[key]
+        for j in image:
+            for key in j.keys():
+                if key == 'id':
+                    assert j[key] == d['image_id']
+                elif key == 'width':
+                    d['width'] = j[key]
+                elif key == 'height':
+                    d['height'] = j[key]
+                elif key == 'file_name':
+                    d['file_name'] = j[key]
+        result.append(d)
+
+with open('reconstruct-anno.json', 'w') as f:
+    json.dump(result, f, indent=4)
